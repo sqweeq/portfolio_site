8adefdf0440dbcf3fcb2d4f976dba170d8358d5d
Katie <katie@KatiedeMacBook-Pro.local>
2020-03-04 13:44:06 +1300

    cleanup file system

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ $("body").append(
   '<div style="" id="loadingDiv"><div class="loader"></div></div>'
 );
 $(window).on("load", function() {
-  setTimeout(removeLoader, 700); //wait for page load PLUS two seconds.
+  setTimeout(removeLoader, 1000); //wait for page load PLUS two seconds.
 });
 function removeLoader() {
   $("#loadingDiv").fadeOut(500, function() {
